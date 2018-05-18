9d637fd96330cf2be2f92601c09847b924d497b9
vinodtonda <vinodtonda@gmail.com>
2018-05-18 22:17:35 +0530

    update ui

---
+++ app.js
@@ -0,0 +1,40 @@
+
+
+window.App = {
+  start: function() {
+    var self = this;
+
+    
+
+    
+
+     
+     
+    };
+  
+
+  
+   
+
+  
+
+  sendCoin: function() {
+    var self = this;
+
+   // var amount = parseInt(document.getElementById("amount").value);
+   // var receiver = document.getElementById("receiver").value;
+
+    this.setStatus("Initiating transaction... (please wait)");
+
+   
+  
+};
+
+window.addEventListener('load', function() {
+ 
+    window.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:9545"));
+ 
+
+  App.start();
+};
+});
